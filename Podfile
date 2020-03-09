ef57c4235ae9f018647fe56097be97a0fdc1b681
Achmad Fachrudin <a2fachrudin@gmail.com>
2020-03-09 14:30:36 +0700

    Initial project

---
+++ Podfile
@@ -0,0 +1,17 @@
+target 'SwiftUIBasic' do
+  use_frameworks!
+
+  pod 'Kingfisher/SwiftUI'
+  pod 'Alamofire'
+
+
+  target 'SwiftUIBasicTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'SwiftUIBasicUITests' do
+    # Pods for testing
+  end
+
+end
---
+++ SwiftUIBasic/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIBasic
+//
+//  Created by Kitabisa.com on 09/03/20.
+//  Copyright © 2020 fachrudin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
