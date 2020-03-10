fa3331b3d384b90ca2aa0dccb8a7f0e66980758d
Achmad Fachrudin <a2fachrudin@gmail.com>
2020-03-10 16:16:08 +0700

    create ListView

---
+++ SwiftUIBasic/View/ListView.swift
@@ -0,0 +1,22 @@
+//
+//  ListView.swift
+//  SwiftUIBasic
+//
+//  Created by Kitabisa.com on 10/03/20.
+//  Copyright © 2020 fachrudin. All rights reserved.
+//
+
+import SwiftUI
+import Alamofire
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ListView_Previews: PreviewProvider {
+    static var previews: some View {
+        ListView()
+    }
+}
